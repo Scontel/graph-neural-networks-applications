221e6140e9b9cfba6900ba49ad0631b6815ab418
Marcus J. Sterling <pcenbestflexilna@gmx.net>
2023-04-02 11:45:00 -0400

    Implement Graph Convolutional Network for node classification on Cora

---
+++ gcn_node_classification.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+
+class GCN(torch.nn.Module):
+    def __init__(self, num_node_features, num_classes):
+        super(GCN, self).__init__()
+        self.conv1 = GCNConv(num_node_features, 16)
+        self.conv2 = GCNConv(16, num_classes)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
+
+def train_cora():
+    dataset = Planetoid(root='/tmp/Cora', name='Cora', transform=T.NormalizeFeatures())
+    data = dataset[0]
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = GCN(dataset.num_node_features, dataset.num_classes).to(device)
+    data = data.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+
+    model.train()
+    for epoch in range(200):
+        optimizer.zero_grad()
+        out = model(data)
+        loss = F.nll_loss(out[data.train_mask], data.y[data.train_mask])
+        loss.backward()
+        optimizer.step()
+        
+        if epoch % 20 == 0:
+            print(f'Epoch {epoch:>3} | Loss: {loss:.4f}')
+
+    model.eval()
+    pred = model(data).argmax(dim=1)
+    correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+    acc = int(correct) / int(data.test_mask.sum())
+    print(f'Test Accuracy: {acc:.4f}')
+
+if __name__ == "__main__":
+    train_cora()
